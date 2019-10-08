40d5f8f0dbf6f3a506518187e1994d6e9e1674d7
Pedro <priccis@gmail.com>
2019-10-08 16:38:27 -0300

    acceptInvite, removeInvites

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,87 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// exports.removeInvite = functions.database
+//   .ref("/users/{userId}/friends/{friendId}")
+//   .onCreate((snapshot, context) => {
+// const userId = context.params.userId;
+// const friendId = context.params.friendId;
+// function getKeyByValue(object, value) {
+//   return Object.keys(object).find(key => object[key] === value);
+// }
+//     admin
+//       .database()
+//       .ref(`/invites/${userId}`)
+//       .once("value")
+//       .then(snap => {
+//         const inviteKey = getKeyByValue(snap.val(), friendId);
+//         return admin
+//           .database()
+//           .ref(`/invites/${userId}/${inviteKey}`)
+//           .remove();
+//       });
+//   });
+
+const emailToId = email => email.replace(".", ",");
+const idToEmail = email => email.replace(",", ".");
+
+exports.acceptInvite = functions.https.onCall((data, context) => {
+  // const uid = context.auth.uid;
+  // const name = context.auth.token.name || null;
+  // const picture = context.auth.token.picture || null;
+  const email = context.auth.token.email || null;
+  console.log("Accepter email:", email);
+
+  const inviterEmail = data.inviterEmail;
+  console.log("Inviter email:", inviterEmail);
+
+  return admin
+    .database()
+    .ref(`/users/${emailToId(email)}/friends`)
+    .push(inviterEmail)
+    .then(() => {
+      return admin
+        .database()
+        .ref(`/users/${emailToId(inviterEmail)}/friends`)
+        .push(email);
+    });
+});
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
+}
+
+function getAllKeysByValue(object, value) {
+  return Object.keys(object).filter(key => object[key] === value);
+}
+
+exports.removeInvitesOnFriendCreation = functions.database
+  .ref("/users/{userId}/friends/{friendId}")
+  .onCreate(async (snapshot, context) => {
+    const userId = context.params.userId;
+    var friendId = "";
+    await snapshot.ref.once("value").then(snap => {
+      friendId = snap.val();
+    });
+
+    const userInvites = admin.database().ref(`/users/${userId}/invites`);
+
+    const removePromises = [];
+    await userInvites.once("value").then(snap => {
+      const invites = snap.val();
+      console.log(invites);
+      console.log(idToEmail(friendId));
+
+      const friendInvites = getAllKeysByValue(invites, idToEmail(friendId));
+      console.log(friendInvites);
+
+      friendInvites.map(inviteId => {
+        removePromises.push(userInvites.child(inviteId).remove());
+      });
+    });
+
+    return Promise.all(removePromises);
+  });
